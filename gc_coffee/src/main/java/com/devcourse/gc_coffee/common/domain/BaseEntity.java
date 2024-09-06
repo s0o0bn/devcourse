41449a638204c753a7e914ab7aa28fb530ea568b
s0o0bn <0921soobin@gmail.com>
2024-09-06 18:29:52 +0900

    🐛 Base, 상품 엔티티 수정

--- gc_coffee/src/main/java/com/devcourse/gc_coffee/common/domain/BaseEntity.java
+++ gc_coffee/src/main/java/com/devcourse/gc_coffee/common/domain/BaseEntity.java
@@ -2,9 +2,9 @@ package com.devcourse.gc_coffee.common.domain;
 
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
-import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreUpdate;
 import lombok.Getter;
+import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.CreationTimestamp;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
@@ -16,12 +16,8 @@ import java.time.LocalDateTime;
 public class BaseEntity {
     @CreationTimestamp
     private LocalDateTime createdAt;
+    @ColumnDefault("NULL")
     private LocalDateTime updatedAt;
-
-    @PrePersist
-    public void onCreate() {
-        updatedAt = null;
-    }
 
     @PreUpdate
     public void onUpdate() {
--- gc_coffee/src/main/java/com/devcourse/gc_coffee/domain/product/Product.java
+++ gc_coffee/src/main/java/com/devcourse/gc_coffee/domain/product/Product.java
@@ -3,20 +3,17 @@ package com.devcourse.gc_coffee.domain.product;
 import com.devcourse.gc_coffee.common.domain.BaseEntity;
 import com.devcourse.gc_coffee.service.product.ProductDto;
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.util.UUID;
 
-@Getter
-@Builder
 @Entity
 @Table(name = "products")
-@NoArgsConstructor
-@AllArgsConstructor
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Product extends BaseEntity {
     @Id
     @Column(name = "product_id")
@@ -24,10 +21,18 @@ public class Product extends BaseEntity {
     private UUID id;
 
     @Column(name = "product_name")
-    @NotNull private String name;
-    @NotNull private String category;
-    @NotNull private long price;
+    private String name;
+    private String category;
+    private long price;
     private String description;
+
+    @Builder
+    public Product(String name, String category, long price, String description) {
+        this.name = name;
+        this.category = category;
+        this.price = price;
+        this.description = description;
+    }
 
     public void update(ProductDto dto) {
         this.name = dto.name();
@@ -36,3 +41,5 @@ public class Product extends BaseEntity {
         this.description = dto.description();
     }
 }
+
+
