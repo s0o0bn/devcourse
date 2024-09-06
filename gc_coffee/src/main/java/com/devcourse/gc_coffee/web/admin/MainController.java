6a8abc1acefb67411e2e8ebd478a81fbab9ec909
s0o0bn <0921soobin@gmail.com>
2024-09-06 16:34:09 +0900

    ✨ 관리자 메인 페이지 추가

---
+++ gc_coffee/src/main/java/com/devcourse/gc_coffee/web/admin/MainController.java
@@ -0,0 +1,14 @@
+package com.devcourse.gc_coffee.web.admin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin")
+public class MainController {
+    @GetMapping("")
+    public String getMainPage() {
+        return "main";
+    }
+}
